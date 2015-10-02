5064b6cabd50f56a568f8b472aeb1b12d1b276c5
ThieryMichel <thiery@marmelab.com>
2015-10-02 14:37:55 +0200

    add docker for wordpress with data only container

---
+++ mariadb/run-mariadb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+VOLUME_HOME="/var/lib/mysql"
+if find ${VOLUME_HOME} -maxdepth 0 -empty | read v; then
+    echo " -> Installation detected in $VOLUME_HOME"
+    echo " -> Installing MariaDB"
+    mysql_install_db > /dev/null 2>&1
+    echo " -> Done!"
+    ./create-mariadb-admin-user.sh
+else
+    echo "-> Booting on existing volume!"
+fi
+
+exec mysqld_safe
