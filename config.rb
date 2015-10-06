6bcc905e1cfd747207ccf806873d4c0a89e1ee5e
ThieryMichel <thiery@marmelab.com>
2015-10-06 16:20:42 +0200

    add docker for compass

---
+++ config.rb
@@ -0,0 +1,15 @@
+# Require any additional compass plugins here.
+
+#Folder settings
+css_dir = "./wp-content/css"          #where the CSS will saved
+sass_dir = "sass"           #where our .scss files are
+#images_dir = "../img"    #the folder with your images
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded # After dev :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
+
+# Obviously
+preferred_syntax = :scss
