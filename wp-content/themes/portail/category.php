73216bb0eb330dac05ad41463fcc652d0b99e375
ThieryMichel <thiery@marmelab.com>
2015-11-25 11:07:39 +0100

    provisory fix for widget layout

--- wp-content/themes/portail/category.php
+++ wp-content/themes/portail/category.php
@@ -36,41 +36,6 @@ get_template_part('header'); ?>
 		}
 	?>
 
-	<h1 class="font-<?php echo $category[0]->category_nicename; ?>">Actualit&eacute;s <?php echo single_cat_title( '', false ); ?></dh1>
-
-<?php foreach($myposts as $post) : ?>
-
-	<?php
-
-	setup_postdata($post);
-
-	$year = mysql2date('Y', $post->post_date);
-	$month = mysql2date('n', $post->post_date);
-	$day = mysql2date('j', $post->post_date);
-
-	?>
-
-	<?php if($year != $previous_year || $month != $previous_month) : ?>
-
-		<?php if($ul_open == true) : ?>
-		</ul>
-		<?php endif; ?>
-
-		<h2><?php the_time('F Y'); ?></h2>
-
-		<ul>
-
-		<?php $ul_open = true; ?>
-
-	<?php endif; ?>
-
-	<?php $previous_year = $year; $previous_month = $month; ?>
-
-	<li><span><?php the_time('j F'); ?> - </span> <span><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></span></li>
-
-<?php endforeach; ?>
-</ul>
-
 	</div>	<!-- fin pagenorm -->
 	<!-- siderbar-right -->
 	<?php if (!$_SESSION['domaine']) {get_template_part('sidebar-right-visite'); }
