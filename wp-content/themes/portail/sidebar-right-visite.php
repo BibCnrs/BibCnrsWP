0c5db7c9a2099c925285421975c60882636727c1
ThieryMichel <thiery@marmelab.com>
2015-12-02 12:35:36 +0100

    remove unneeded files

--- wp-content/themes/portail/sidebar-right-visite.php
+++
@@ -1,6 +0,0 @@
-<div id="sidebar">
-	<?php if( !dynamic_sidebar( 'sidebar-right-visite' ) ) : ?>
-			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-f4') )?> 			
-			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-f5') )?> 			
-	<?php endif; ?>
-</div>
