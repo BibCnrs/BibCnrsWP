da88292fee498f103d1fed5eecffc6906c762134
ThieryMichel <thiery@marmelab.com>
2015-12-02 18:45:05 +0100

    add BibCnrsPosts class

--- wp-content/themes/portail/category.php
+++ wp-content/themes/portail/category.php
@@ -4,9 +4,14 @@ Template Name: category
 */
 /*  Connexion */
 require 'config.php';
+
 require 'models/BibCnrsDomain.php';
 $domain = new BibCnrsDomain($config['category']['domains'], $config['category']['news']);
+
 force_login($domain->currentDomain, $config['category']['domains']);
+
+require 'models/BibCnrsPosts.php';
+$bibCnrsPosts = new BibCnrsPosts($config['category']['domains']);
 
 /* Display */
 $context = Timber::get_context();
@@ -14,14 +19,7 @@ $context['visit'] = $domain->visit;
 $context['currentDomain'] = $domain->currentDomain;
 $context['userDomain'] = $domain->userDomain;
 
-$context['postsdomain'] = Timber::get_posts(['category_name' => $domain->currentDomain->slug ]);
+$context['postsdomain'] = $bibCnrsPosts->getPostsFor($domain->currentDomain);
+$context['alltheposts'] = $bibCnrsPosts->getPostsNotIn($domain->currentDomain, 5);
 
-$domainIds = [];
-foreach ($config['category']['domains'] as $value) {
-    $cat = get_category_by_slug($value);
-    if ($cat->slug != $context['currentDomain']->slug) {
-        $domainIds[] = get_category_by_slug($value)->term_id;
-    }
-}
-$context['alltheposts'] = Timber::get_posts(array( 'category__in' => $domainIds, 'showposts' => '5'));
 Timber::render('category.twig', $context);
--- wp-content/themes/portail/single.php
+++ wp-content/themes/portail/single.php
@@ -4,10 +4,13 @@ Template Name: single
 */
 /*  Connexion */
 require 'config.php';
-require 'models/BibCnrsDomain.php';
 
+require 'models/BibCnrsDomain.php';
 $domain = new BibCnrsDomain($config['category']['domains'], $config['category']['news']);
 force_login($domain->currentDomain, $config['category']['domains']);
+
+require 'models/BibCnrsPosts.php';
+$posts = new BibCnrsPosts($config['category']['domains']);
 
 /* Display */
 $context = Timber::get_context();
@@ -17,11 +20,9 @@ $context['currentDomain'] = $domain['currentDomain'];
 $context['institute'] = $domain['institute'];
 $context['userDomain'] = $domain['userDomain'];
 
-$context['postsdomain'] = Timber::get_posts(['category_name' => $domain['userDomain']->name ]);
-$context['post'] = new TimberPost();
+$context['post'] = $posts->getCurrentPost();
 
-foreach ($config['category']['domains'] as $value){
-    $domainIds[] = get_category_by_slug($value)->term_id;
-}
-$context['alltheposts'] = Timber::get_posts(array( 'category__in' => $domainIds, 'showposts' => '5'));
+$context['postsdomain'] = $posts->getPostsFor($domain->currentDomain);
+$context['alltheposts'] = $posts->getPostsNotIn($domain->currentDomain, 5);
+
 Timber::render('single.twig', $context);
