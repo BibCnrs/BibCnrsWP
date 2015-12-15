160744212114419711afba6926a27f9f0a7bb913
ThieryMichel <thiery@marmelab.com>
2015-12-15 15:25:29 +0100

    change domain of widget based on current domain of page

--- wp-content/themes/portail/category.php
+++ wp-content/themes/portail/category.php
@@ -23,6 +23,7 @@ $context = Timber::get_context();
 $context['currentCategory'] = $currentCategory;
 $context['userCategory'] = $userCategory;
 $context['visit'] = $currentCategory->slug != $userCategory->slug;
+$context['ebsco_widget'] = '[ebsco_widget domain="' . $config['profile_map'][$currentCategory->slug] . '"]';
 
 $context['categoryPosts'] = $postsProvider->getPostsFor($currentCategory);
 $context['allOtherPosts'] = $postsProvider->getPostsNotIn($currentCategory, 5);
