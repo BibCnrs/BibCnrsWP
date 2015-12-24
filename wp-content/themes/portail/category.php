987d91158cc1b082d8673a3e0671348644d29a68
patrickkremer <patrick.kremer@inist.fr>
2015-12-24 15:27:38 +0100

    add logo to second pages

--- wp-content/themes/portail/category.php
+++ wp-content/themes/portail/category.php
@@ -27,5 +27,4 @@ $context['ebsco_widget'] = '[ebsco_widget domain="' . $config['profile_map'][$cu
 
 $context['categoryPosts'] = $postsProvider->getPostsFor($currentCategory);
 $context['allOtherPosts'] = $postsProvider->getPostsNotIn($currentCategory, 5);
-
 Timber::render('category.twig', $context);
