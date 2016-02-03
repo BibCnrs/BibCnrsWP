20dacec3ededf44b2cab1303de26af2f5dd80697
patrickkremer <patrick.kremer@inist.fr>
2016-02-03 15:24:27 +0100

    databases lists

--- wp-content/themes/portail/home.php
+++ wp-content/themes/portail/home.php
@@ -1,13 +1,15 @@
 <?php
 /*Template Name: Home*/
+require 'config.php';
 $context = Timber::get_context();
-$slugs = array('une', 'formations', 'infosist'); //category list by slug
+$slugs = $config['category']['news']; //category list by slug
 $context['news'] = array();
 foreach($slugs as $slug){
 	$context['news'][] = [
 		'title' => get_category_by_slug($slug)->name,
 		'slug' => $slug,
-		'posts' => Timber::get_posts(['category_name' => $slug, 'numberposts' => 5])
+		'posts' => Timber::get_posts(['category_name' => $slug, 'numberposts' => 3])
 	];
 }
 Timber::render('homepage.twig', $context);
+?>
