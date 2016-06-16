c04d818e2d581cd314f56eb55b893ce77d2dd34e
ThieryMichel <thiery@marmelab.com>
2016-06-16 18:14:59 +0200

    fix init script enqueueing

--- wp-content/themes/portail/js/BibHeaderWidget.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-document.onreadystatechange = function () {
-    if (document.readyState === 'complete') {
-        var widgetElement = document.getElementById('BibHeader');
-        window.ReactDOM.render(window.React.createElement(window.bibcnrsWidget.BibHeader), headerElement);
-    }
-};
