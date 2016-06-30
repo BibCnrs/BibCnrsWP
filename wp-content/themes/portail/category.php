ec08024b8568341d2de13ece742740002dd44dce
patrickkremer <patrick.kremer@inist.fr>
2016-06-30 15:34:26 +0200

    faq with subcategories

--- wp-content/themes/portail/category.php
+++ wp-content/themes/portail/category.php
@@ -20,15 +20,32 @@ $postsProvider = new BibCnrsPostsProvider($config['category']['domains'], 'get_c
 
 /* Display */
 $context = Timber::get_context();
-$preferences="pref-".$currentCategory->slug;
-$context['pref'] = Timber::get_posts(array('category_name' => $preferences));
-$context['currentCategory'] = $currentCategory;
-$context['userCategory'] = $userCategory;
-$context['visit'] = $currentCategory->slug != $userCategory->slug;
-$context['other'] = in_array($currentCategory->slug,$config['category']['domains']);
 $context['ebsco_widget'] = '[ebsco_widget domain="' . $config['profile_map'][$currentCategory->slug] . '"]';
 
-$context['categoryPosts'] = $postsProvider->getPostsFor($currentCategory);
-$context['allOtherPosts'] = $postsProvider->getPostsNotIn($currentCategory, 5);
-Timber::render('category.twig', $context);
+// IF FAQ other display and sub-categories
+$parentCatName = single_cat_title('',false);
+if ($parentCatName == "F.A.Q." OR $parentCatName == "FAQ"){
+    $parentCatID = get_cat_ID($parentCatName);
+    $slugs = get_categories( 'child_of='.$parentCatID );
+    foreach($slugs as $slug){
+        $nom=$slug->slug;
+    	$context['faqPosts'][] = [
+            'title' => get_category_by_slug($nom)->name,
+    		'slug' => $slug,
+    		'posts' => Timber::get_posts(array('category_name' => $nom))
+    	];
+    }
+    Timber::render('faq.twig', $context);
+}
+else{
+    $preferences="pref-".$currentCategory->slug;
+    $context['pref'] = Timber::get_posts(array('category_name' => $preferences));
+    $context['currentCategory'] = $currentCategory;
+    $context['userCategory'] = $userCategory;
+    $context['visit'] = $currentCategory->slug != $userCategory->slug;
+    $context['other'] = in_array($currentCategory->slug,$config['category']['domains']);
+    $context['categoryPosts'] = $postsProvider->getPostsFor($currentCategory);
+    $context['allOtherPosts'] = $postsProvider->getPostsNotIn($currentCategory, 5);
+    Timber::render('category.twig', $context);
+}
 ?>
---
+++ wp-content/themes/portail/js/flipflop.js
@@ -0,0 +1,6 @@
+function flipflop(id)
+{
+	if (document.getElementById(id).style.display == "none")
+			document.getElementById(id).style.display = "block";
+	else	document.getElementById(id).style.display = "none";
+}
