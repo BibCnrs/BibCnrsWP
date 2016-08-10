2d38232c9da614220ba0574f88447b9b8c746d59
ThieryMichel <thiery@marmelab.com>
2016-08-10 15:03:27 +0200

    fix domain name in profile_map

--- wp-content/themes/portail/js/BibHeaderInit.js
+++ wp-content/themes/portail/js/BibHeaderInit.js
@@ -1,10 +1,4 @@
 "use strict";
 
-console.log('im loaded');
-// document.onreadystatechange = function () {
-console.log('onreadystatechange');
-    // if (document.readyState === 'complete') {
-        var widgetElement = document.getElementById('BibHeader');
-        window.ReactDOM.render(window.React.createElement(window.bibcnrsWidget.BibHeader), widgetElement);
-    // }
-// };
+var widgetElement = document.getElementById('BibHeader');
+window.ReactDOM.render(window.React.createElement(window.bibcnrsWidget.BibHeader), widgetElement);
