a7bfc51aa9b49d215b5d89557d2e29f366590e3c
ThieryMichel <thiery@marmelab.com>
2018-02-21 15:06:14 +0100

    translate title

--- wp-content/themes/portail/404.php
+++ wp-content/themes/portail/404.php
@@ -1,4 +1,13 @@
 <?php
 $context = Timber::get_context();
 $context['robot_index'] = $_ENV['ROBOT_INDEX'];
+$language = substr($context['site']->language, 0, 2);
+$context['long_title'] = $language === 'fr' ?
+        'Accès aux ressources documentaires des unités de recherche du CNRS'
+    :
+        'CNRS research units documents access';
+$context['short_title'] = $language === 'fr' ?
+        'Accès aux ressources documentaires du CNRS'
+    :
+        'CNRS documents access';
 Timber::render('404.twig', $context);
--- wp-content/themes/portail/home.php
+++ wp-content/themes/portail/home.php
@@ -4,6 +4,14 @@ require 'config.php';
 $context = Timber::get_context();
 $context['robot_index'] = $_ENV['ROBOT_INDEX'];
 $language = substr($context['site']->language, 0, 2);
+$context['long_title'] = $language === 'fr' ?
+        'Accès aux ressources documentaires des unités de recherche du CNRS'
+    :
+        'CNRS research units documents access';
+$context['short_title'] = $language === 'fr' ?
+        'Accès aux ressources documentaires du CNRS'
+    :
+        'CNRS documents access';
 $domain = $config['profile_map'][$currentCategory->description];
 $dbUrl = $language === 'fr' ? '\/bases-de-donnees\/' : '\/data-bases\/';
 $context['ebsco_widget'] = sprintf('[ebsco_widget domain="%s" language="%s" db_url="%s"]', $domain, $language, $dbUrl);
