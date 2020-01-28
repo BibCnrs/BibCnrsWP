4348800b326fe908a9bfb55ad4e776d2bbc4601a
fjoin <florence.join@gmail.com>
2020-01-28 10:25:39 +0100

    discover and news

--- wp-content/themes/portail/js/discovery.js
+++ wp-content/themes/portail/js/discovery.js
@@ -2,26 +2,45 @@
     // DOM ready
     $(function() {
 		function change_domain(){
-			$('.newsBox').hide();
+			$('.discBox').hide();
 			$('#domain-'+encours+'').hide();
 			$('#domain-'+prochaine+'').show();
-			var elems = $('.newsBox[data-type="type-'+prochaine+'"]');
-			$('#detail-'+prochaine+' .newsBox').show();
+			var elems = $('.discBox[data-type="type-'+prochaine+'"]');
+			$('#detail-'+prochaine+' .discBox').css({"display":"flex"});
 			$('#menuNews-'+encours).prop( "checked", false );
 			$('#menuNews-'+prochaine).prop( "checked", true );
 			encours = prochaine;
 		}
-		$('.newsBox').hide();
+		function show_detail(){
+			$('.postDetail').hide();
+			$('.discBox').css({"border":"none"});
+			$('#postDetail-'+focus+'').show();
+			$('#box-'+focus+'').css({"border":"2px solid #FF9982"});
+		}
+		function close_detail(){
+			$('#postDetail-'+focusnext+'').hide();
+			$('#box-'+focus+'').css({"border":"none"});
+		}
 		var encours = $('.menuItem').first().attr('id').split("-")[1];
 		var prochaine;
-		var elems = $('.newsBox[data-type="type-'+encours+'"]');
+		var focus;
+		var focusnext;
+		var elems = $('.discBox[data-type="type-'+encours+'"]');
 		$('#domain-'+encours+'').show();
-		$('#detail-'+encours+' .newsBox').show();
+		$('#detail-'+encours+' .discBox').css({"display":"flex"});
 		$('#menuNews-'+encours).prop( "checked", true );
 
 		$('.menuItem').click(function(e) {
 			prochaine = $(e.target).attr('id').split("-")[1];
 			change_domain();
+		});
+		$('.discDetail').click(function(e) {
+			focus = $(e.target).attr('id').split("-")[1];
+			show_detail();
+		});
+		$('.detailClose').click(function(e) {
+			focusnext = $(e.target).attr('id').split("-")[1];
+			close_detail();
 		});
     });
 })(jQuery);
--- wp-content/themes/portail/js/news.js
+++ wp-content/themes/portail/js/news.js
@@ -11,6 +11,8 @@
 			$('.newsBox').hide();
 			$('#domain-'+encours+'').hide();
 			$('#domain-'+prochaine+'').show();
+			$('#title-'+encours+'').hide();
+			$('#title-'+prochaine+'').show();
 			var elems = $('.newsBox[data-type="type-'+prochaine+'"]');
 			var nbpage = Math.ceil(elems.length/postperpage);
 			$('#newsPages').children('span').remove();
@@ -23,20 +25,33 @@
 				elems.slice(i,i+postperpage).wrapAll('<div class="flexrow" id="grpNews-'+prochaine+'-'+grpnews+'"> ')
 				grpnews = grpnews + 1;
 			}
-			$('#grpNews-'+prochaine+'-1 .newsBox').show();
+			$('#grpNews-'+prochaine+'-1 .newsBox').css({"display":"flex"});
 			$('#menuNews-'+encours).prop( "checked", false );
 			$('#menuNews-'+prochaine).prop( "checked", true );
 			encours = prochaine;
 		}
 		function change_page(){
 			$('.newsBox').hide();
-			$('#grpNews-'+encours+'-'+nextpage+' .newsBox').show();
+			$('#grpNews-'+encours+'-'+nextpage+' .newsBox').css({"display":"flex"});
+		}
+		function show_detail(){
+			$('.postDetail').hide();
+			$('.newsBox').css({"border":"none"});
+			$('#domain-'+encours+' #postDetail-'+focus+'').show();
+			$('#detail-'+encours+' #box-'+focus+'').css({"border":"2px solid #FF9982"});
+		}
+		function close_detail(){
+			console.log(encours);
+			$('#domain-'+encours+' #postDetail-'+focusnext+'').hide();
+			$('#detail-'+encours+' #box-'+focus+'').css({"border":"none"});
 		}
 		var encours = $('.menuItem').first().attr('id').split("-")[1];
 		var grpnews = 1;
 		var prochaine;
 		var encourspage = 1;
 		var nextpage;
+		var focus;
+		var focusnext;
 		var postperpage =12;
 		var elems = $('.newsBox[data-type="type-'+encours+'"]');
 		var nbpage = Math.ceil(elems.length/postperpage);
@@ -44,13 +59,13 @@
 			$('#newsPages').append('<span class="newsPage" id="page-'+j+'">'+j+'</span>');
 		}
 		$('#page-'+encourspage+'').toggleClass('activePage');
-		$('.newsBox').hide();
 		for (var i = 0; i < elems.length; i+=postperpage) {
 			elems.slice(i,i+postperpage).wrapAll('<div class="flexrow" id="grpNews-'+encours+'-'+grpnews+'"> ')
 			grpnews = grpnews + 1;
 		}
-		$('#grpNews-'+encours+'-1 .newsBox').show();
+		$('#grpNews-'+encours+'-1 .newsBox').css({"display":"flex"});;
 		$('#domain-'+encours+'').show();
+		$('#title-'+encours+'').show();
 		$('#menuNews-'+encours).prop( "checked", true );
 
 		$('.menuItem').click(function(e) {
@@ -64,6 +79,14 @@
 			$('#page-'+nextpage+'').toggleClass('activePage');
 			encourspage = nextpage;
 			change_page();
+		});
+		$('.discDetail').click(function(e) {
+			focus = $(e.target).attr('id').split("-")[1];
+			show_detail();
+		});
+		$('.detailClose').click(function(e) {
+			focusnext = $(e.target).attr('id').split("-")[1];
+			close_detail();
 		});
     });
 })(jQuery);
